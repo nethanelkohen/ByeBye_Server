7ab05d08a3369a882fd67d01fec74ec33b47abf5
Nethanel Kohen <nethanel.kohen@gmail.com>
2017-12-23 20:44:14 -0500

    heroku test cli 2

--- index.js
+++ index.js
@@ -25,10 +25,7 @@ if (!isConfigured) {
 }
 
 const twilio = require('twilio');
-const client = new twilio(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
+const client = new twilio(cfg.accountSid, cfg.authToken);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
@@ -37,7 +34,7 @@ app.get('/api/byebye', function(req, res) {
     .create({
       body: 'Hello from Node',
       to: '+19176075745',
-      from: process.env.TWILIO_NUMBER
+      from: cfg.sendingNumber
     })
     .then(message => console.log(message.sid))
     .then(res.send('Message sent'));
