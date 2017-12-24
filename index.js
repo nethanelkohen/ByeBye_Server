55ae661d9d8468a7b0f5a68dda209aa92c327182
Nethanel Kohen <nethanel.kohen@gmail.com>
2017-12-23 20:12:23 -0500

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const config = require('./config');
-const port = process.env.PORT || 3000;
 
 const twilio = require('twilio');
 const client = new twilio(
@@ -11,7 +10,7 @@ const client = new twilio(
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/byebye', function(req, res) {
+app.get('/api/byebye', function(req, res) {
   client.messages
     .create({
       body: 'Hello from Node',
@@ -22,5 +21,6 @@ app.get('/byebye', function(req, res) {
     .then(res.send('Message sent'));
 });
 
-console.log('listening on', port);
-app.listen(port);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Your server is available at localhost:3000!');
+});
